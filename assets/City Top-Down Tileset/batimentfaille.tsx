b70b7858755a7ebb4d5914c4dbff47bd6ea3ac14
MathTek <mathieu.rasoanaivo@epitech.eu>
2024-01-20 17:34:06 +0400

    Feat: add tsx

---
+++ assets/City Top-Down Tileset/batimentfaille.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="batimentfaille" tilewidth="32" tileheight="32" tilecount="572" columns="26">
+ <image source="Downloads/City Top-Down Tileset/City Top-Down Tileset/example.png" width="863" height="720"/>
+</tileset>
